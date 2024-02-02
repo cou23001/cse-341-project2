f9d90a8b0e4e9a9dd4e5586a39b900ec824964f9
Cesar Coutino <ccoutino@gmail.com>
2024-02-02 11:05:17 -0700

    Cleaning

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const users = require('../controllers/user');
+const validation = require('../middleware/validate');
 
-router.post('/', users.createUser);
+
+router.post('/', validation.saveUser,users.createUser);
 
 module.exports = router;
